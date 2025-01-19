10cbd1efb728235ef9810111c31d6b04624c4ccc
Chatuye <Chatuye@proton.me>
2025-01-19 10:01:34 +0100

    Revert "second-major-refactoring-part-1"

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 var mainBody = null;
+var cards = new Array();
 var stage = null;
-var mainViewPort = null;
+var spots = null;
+
+var UIresizing = false;
+
+var draggedCard = null;
+var draggedObject = null;
+
+var tob = null;
 
 function onBodyLoad() {
 	mainBody = document.getElementById("mainBody");
 
-	mainViewPort = new ViewPort(null, "relative", 2.0, 2.0);
-	stage = new Stage(mainBody, "relative", 0.05, 0.05, "relative", 0.9, 0.9, "relative", 1.0, 1.0);
+	stage = new CardStage(mainBody);
 }
 
 function onBodyResize() {
-	mainViewPort.width = mainBody.getBoundingClientRect().width;
-	mainViewPort.height = mainBody.getBoundingClientRect().height;
+	UIresizing = true;
 	
-	stage.onParentChange();
+	stage.onResize();
+	
+	UIresizing = false;
 }
 
-function randomHexColorCode() {
-	let n = (Math.random() * 0xfffff * 1000000).toString(16);
-	return '#' + n.slice(0, 6);
-};
+function logVar(s, v) {
+	console.log(s+": "+v);
+}
